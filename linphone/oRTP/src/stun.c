6e2723782a161391fdc8d8e2372d80e83c840850
aymeric <aymeric@3f6dc0c8-ddfe-455d-9043-3cd528dc4637>
2009-03-07 11:21:33 +0000

    fix gcc compilation
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/linphone/trunk@305 3f6dc0c8-ddfe-455d-9043-3cd528dc4637

--- linphone/oRTP/src/stun.c
+++ linphone/oRTP/src/stun.c
@@ -1355,7 +1355,7 @@ stunCalculateFingerprint(const char* input, int length)
 		0x54de5729, 0x23d967bf, 0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94,
 		0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d
 	};
-	const uint8_t *p = input;
+	const uint8_t *p = (uint8_t*)input;
 	uint32_t crc;
 
 	crc = ~0U;
