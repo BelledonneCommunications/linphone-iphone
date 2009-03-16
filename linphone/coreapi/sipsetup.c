f08a56d4034f0fb153694bb0d9fd92906af0075a
smorlat <smorlat@3f6dc0c8-ddfe-455d-9043-3cd528dc4637>
2009-03-16 14:57:09 +0000

    fix make distcheck disable static plugin by default
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/linphone/trunk@347 3f6dc0c8-ddfe-455d-9043-3cd528dc4637

--- linphone/coreapi/sipsetup.c
+++ linphone/coreapi/sipsetup.c
@@ -17,12 +17,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-
-#include "linphonecore.h"
-
 #ifdef HAVE_CONFIG_H
 #include "../config.h"
 #endif
+
+#include "linphonecore.h"
 
 #ifndef LINPHONE_PLUGINS_DIR
 #ifdef WIN32
