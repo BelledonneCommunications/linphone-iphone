54174c222c192111c4c8ed6a3b251e21b6a8a3ba
simorl <simon.morlat@linphone.org>
2009-06-21 21:38:37 +0200

    - make zip works - buddylookup plugin compiles.

--- linphone/coreapi/sipsetup.c
+++ linphone/coreapi/sipsetup.c
@@ -23,14 +23,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "linphonecore.h"
 
-#ifndef LINPHONE_PLUGINS_DIR
-#ifdef WIN32
-#define LINPHONE_PLUGINS_DIR "liblinphone\\plugins\\"
-#else
-#define LINPHONE_PLUGINS_DIR "."
-#endif
-#endif
-
 static SipSetup *all_sip_setups[]={
 	NULL
 };
