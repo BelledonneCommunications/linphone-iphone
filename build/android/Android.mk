5327a1b00ed620169cad6fcba7178e307ecd42df
Jehan Monnier <jehan.monnier@linphone.org>
2010-04-02 16:28:44 +0200

    add android build script + linphone core java interfaces

---
+++ build/android/Android.mk
@@ -0,0 +1,90 @@
+##
+## Android.mk -Android build script-
+##
+##
+## Copyright (C) 2010  Belledonne Communications, Grenoble, France
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU Library General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program; if not, write to the Free Software
+##  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+##
+
+LOCAL_PATH:= $(call my-dir)/../../coreapi
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := liblinphone
+
+LOCAL_CPP_EXTENSION := .cc
+
+LOCAL_SRC_FILES = \
+	linphonecore.c \
+	misc.c  \
+	enum.c \
+	enum.h \
+	presence.c \
+	proxy.c \
+	friend.c \
+	authentication.c \
+	lpconfig.c \
+	chat.c \
+	general_state.c \
+	sipsetup.c \
+	siplogin.c \
+	address.c \
+	linphonecore_jni.cc \
+	sal.c \
+	sal_eXosip2.c \
+	sal_eXosip2_presence.c \
+	sal_eXosip2_sdp.c \
+	offeranswer.c \
+	callbacks.c
+
+LOCAL_CFLAGS += \
+	-D_BYTE_ORDER=_LITTLE_ENDIAN \
+	-DORTP_INET6 \
+	-DENABLE_TRACE \
+	-DLINPHONE_VERSION=\"Linphone-3.1.2\" \
+	-DLINPHONE_PLUGINS_DIR=\"\\tmp\" \
+	-DLOG_DOMAIN=\"Linphone\"
+
+LOCAL_CFLAGS += -DIN_LINPHONE
+#LOCAL_CFLAGS += -DVIDEO_ENABLED -DIN_LINPHONE
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH) \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/../oRTP/include \
+	$(LOCAL_PATH)/../mediastreamer2/include \
+	$(LOCAL_PATH)/../../../eXosip/include \
+	$(LOCAL_PATH)/../../../osip/include 
+
+LOCAL_LDLIBS += -llog
+
+LOCAL_STATIC_LIBRARIES := \
+	libmsandroidsnd \
+	libmediastreamer2 \
+	libortp \
+	libspeex \
+	libeXosip2 \
+	libosip2 \
+	libgsm
+
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+
+
+
+
