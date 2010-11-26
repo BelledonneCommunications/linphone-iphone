f8db2b8f80ca5b365b783566204bc59e7953bb68
Jehan Monnier <jehan.monnier@linphone.org>
2010-11-26 20:03:52 +0100

    remove java 1.3 compilation issue

--- java/common/org/linphone/core/VideoSize.java
+++ java/common/org/linphone/core/VideoSize.java
@@ -58,7 +58,6 @@ public final class VideoSize {
 	}
 	
 	// Generated
-	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
@@ -66,7 +65,6 @@ public final class VideoSize {
 		result = prime * result + width;
 		return result;
 	}
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
