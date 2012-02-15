0de57c7d280bdb3558c3bad0c9e3f8570805b991
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2012-02-15 14:12:08 +0100

    Fix ipa rule

--- submodules/build/builder-iphone-os.mk
+++ submodules/build/builder-iphone-os.mk
@@ -326,7 +326,7 @@ delivery:
 	-x linphone-iphone/build\* \
 	-x \*.git\*
 
-ipa: all
+ipa:
 	cd $(BUILDER_SRC_DIR)/../ \
 	&& xcodebuild  -configuration DistributionAdhoc \
 	&& xcrun -sdk iphoneos PackageApplication -v build/DistributionAdhoc-iphoneos/linphone.app -o $(BUILDER_SRC_DIR)/../linphone-iphone.ipa
