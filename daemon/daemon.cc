337eb3972013a093a23b59ffed6b8d7e83ea34f8
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2012-05-25 09:54:14 +0200

    Fix mixup between capture/display in daemon

--- daemon/daemon.cc
+++ daemon/daemon.cc
@@ -212,7 +212,7 @@ Daemon::Daemon(const char *config_path, const char *factory_config_path, const c
 	vtable.dtmf_received = dtmfReceived;
 	mLc = linphone_core_new(&vtable, config_path, factory_config_path, this);
 	linphone_core_set_user_data(mLc, this);
-	linphone_core_enable_video(mLc, display_video, capture_video);
+	linphone_core_enable_video(mLc, capture_video, display_video);
 	linphone_core_enable_echo_cancellation(mLc, false);
 	initCommands();
 }
