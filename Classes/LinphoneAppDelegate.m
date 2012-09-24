421a1a390fcf3ea61d5054114e560751e8d388f9
Jehan Monnier <jehan.monnier@linphone.org>
2012-09-24 18:54:52 +0200

    fix GSM call handling

--- Classes/LinphoneAppDelegate.m
+++ Classes/LinphoneAppDelegate.m
@@ -60,19 +60,12 @@
     CTCallCenter* ct = (CTCallCenter*) cCenter;
     
     int callCount = [ct.currentCalls count];
-    if (!callCount) {
-        [LinphoneLogger logc:LinphoneLoggerLog format:"No GSM call -> enabling SIP calls"];
-        linphone_core_set_max_calls([LinphoneManager getLc], 3);
-    } else {
-        [LinphoneLogger logc:LinphoneLoggerLog format:"%d GSM call(s) -> disabling SIP calls", callCount];
-        /* pause current call, if any */
-        LinphoneCall* call = linphone_core_get_current_call([LinphoneManager getLc]);
-        if (call) {
-            [LinphoneLogger logc:LinphoneLoggerLog format:"Pausing SIP call"];
-            linphone_core_pause_call([LinphoneManager getLc], call);
-        }
-        linphone_core_set_max_calls([LinphoneManager getLc], 0);
-    }
+     /* pause current call, if any */
+     LinphoneCall* call = linphone_core_get_current_call([LinphoneManager getLc]);
+     if (callCount>0 && call) {
+          [LinphoneLogger logc:LinphoneLoggerLog format:"Pausing SIP call"];
+          linphone_core_pause_call([LinphoneManager getLc], call);
+      }
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application{
