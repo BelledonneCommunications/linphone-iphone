f6038bf3ef76588a8b71ddfb0e453364b2de89ba
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2013-03-15 10:19:41 +0100

    Fix bug with the keywordcmp macro.
    This macro was supposed to be passed a string literal as first argument but was used with a string pointer sometimes.

--- coreapi/sal_eXosip2_sdp.c
+++ coreapi/sal_eXosip2_sdp.c
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "sal.h"
 #include <eXosip2/eXosip.h>
 
-#define keywordcmp(key,b) strncmp(key,b,sizeof(key))
+#define keywordcmp(key,b) strcmp(key,b)
 
 #ifdef FOR_LATER
 
