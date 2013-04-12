7d4a50043e3f39735f85d9491470d9c5d8229252
Jehan Monnier <jehan.monnier@linphone.org>
2013-04-12 08:58:57 +0200

    fix various compilation issue

--- gtk/logging.c
+++ gtk/logging.c
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <sys/stat.h>
 #include <sys/types.h>
 #endif
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
 extern gchar *linphone_logfile;
 
