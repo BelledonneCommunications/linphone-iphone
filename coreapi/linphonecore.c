cc671644bea7dd3426316636ab6ce24526683374
Johan Pascal <johan.pascal@belledonne-communications.com>
2013-04-21 22:06:07 +0200

    Add aac-eld paylaod type to linphone core. - add two configurations with their respective fmtp config string

--- coreapi/linphonecore.c
+++ coreapi/linphonecore.c
@@ -1291,6 +1291,8 @@ static void linphone_core_init (LinphoneCore * lc, const LinphoneCoreVTable *vta
 	linphone_core_assign_payload_type(lc,&payload_type_silk_wb,-1,NULL);
 	linphone_core_assign_payload_type(lc,&payload_type_silk_swb,-1,NULL);
 	linphone_core_assign_payload_type(lc,&payload_type_g729,18,"annexb=no");
+	linphone_core_assign_payload_type(lc,&payload_type_aaceld_nb,-1,"config=F8EE2000; constantDuration=512;  indexDeltaLength=3; indexLength=3; mode=AAC-hbr; profile-level-id=24; sizeLength=13; streamType=5");
+	linphone_core_assign_payload_type(lc,&payload_type_aaceld_wb,-1,"config=F8E82000; constantDuration=512;  indexDeltaLength=3; indexLength=3; mode=AAC-hbr; profile-level-id=24; sizeLength=13; streamType=5");
 	linphone_core_handle_static_payloads(lc);
 	
 	ms_init();
