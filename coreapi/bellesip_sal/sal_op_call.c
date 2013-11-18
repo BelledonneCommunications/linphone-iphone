d5a091ba0a904fcd0de7fe89b5c2ebaefa8ff0b2
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2013-11-18 11:33:47 +0100

    Updated README + fix SIP_INFO DTMF crash when trying to send a DTMF while call is ringing

--- coreapi/bellesip_sal/sal_op_call.c
+++ coreapi/bellesip_sal/sal_op_call.c
@@ -693,7 +693,7 @@ SalMediaDescription * sal_call_get_final_media_description(SalOp *h){
 }
 
 int sal_call_send_dtmf(SalOp *h, char dtmf){
-	if (h->dialog){
+	if (h->dialog && (belle_sip_dialog_get_state(h->dialog) == BELLE_SIP_DIALOG_CONFIRMED || belle_sip_dialog_get_state(h->dialog) == BELLE_SIP_DIALOG_EARLY)){
 		belle_sip_request_t *req=belle_sip_dialog_create_queued_request(h->dialog,"INFO");
 		if (req){
 			int bodylen;
