08a7bf3be5f49703a4c128a01ff9c8950d1cb23c
Simon Morlat <simon.morlat@linphone.org>
2013-12-18 16:31:00 +0100

    improve mac bundle generation

--- build/macos/environment.sh
+++ build/macos/environment.sh
@@ -2,4 +2,5 @@ export EXTRA_ARGS="--workdir $bundle_res"
 export GIO_EXTRA_MODULES="$bundle_lib/gio/modules"
 export PANGO_LIBDIR="$bundle_lib"
 export PANGO_SYSCONFDIR="$bundle_etc"
+export DYLD_LIBRARY_PATH=
 
