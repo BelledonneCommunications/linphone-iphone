71130788178fbe5dcfddc7d703d3257cadc1b14f
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2014-02-20 17:30:30 +0100

    Fix polarssl compilation nightmare

--- submodules/build/builders.d/polarssl.mk
+++ submodules/build/builders.d/polarssl.mk
@@ -7,20 +7,16 @@ BUILD_DIR=$(BUILDER_BUILD_DIR)
 $(SRC_DIR)/$(polarssl_dir)/configure:
 	cd $(SRC_DIR)/$(polarssl_dir) && ./autogen.sh
 
-$(SRC_DIR)/$(polarssl_dir)/Makefile: $(SRC_DIR)/$(polarssl_dir)/configure
+$(BUILD_DIR)/$(polarssl_dir)/Makefile: $(SRC_DIR)/$(polarssl_dir)/configure
 	mkdir -p $(BUILD_DIR)/$(polarssl_dir)
-	cd $(BUILD_DIR)/$(polarssl_dir)/ \
+	cd $(BUILD_DIR)/$(polarssl_dir) \
 	&& PKG_CONFIG_LIBDIR=$(prefix)/lib/pkgconfig CONFIG_SITE=$(SRC_DIR)/build/$(config_site) \
 	$(SRC_DIR)/$(polarssl_dir)/configure --prefix=$(prefix) --host=$(host) ${library_mode}  
 
-update-tree:  $(SRC_DIR)/$(polarssl_dir)/Makefile
-	mkdir -p $(BUILD_DIR)/$(polarssl_dir)
-	cd $(BUILD_DIR)/$(polarssl_dir)/ && \
-	rsync -rvLpgoc --exclude ".git" $(SRC_DIR)/$(polarssl_dir)/ .
-
-build-polarssl: update-tree
-	host_alias=$(host) && . /$(SRC_DIR)/build/$(config_site) && \
-	cd $(BUILD_DIR)/$(polarssl_dir) && make && make install
+build-polarssl: $(BUILD_DIR)/$(polarssl_dir)/Makefile
+	cd $(BUILD_DIR)/${polarssl_dir} && \
+	host_alias=$(host) && . $(SRC_DIR)/build/$(config_site) && \
+	make && make install
 
 clean-polarssl:
 	-cd $(BUILD_DIR)/$(polarssl_dir) && make clean
