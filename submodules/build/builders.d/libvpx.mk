5ab859fd08536af373bfd8f7d75c3c5a85b5754d
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2014-03-27 09:36:11 +0100

    Apply the proposed fix for the garbling of vp8 here: https://code.google.com/p/webm/issues/detail?id=603#c45

--- submodules/build/builders.d/libvpx.mk
+++ submodules/build/builders.d/libvpx.mk
@@ -2,7 +2,7 @@ libvpx_configure_options=\
 	--enable-static   --disable-shared \
 	--enable-error-concealment --disable-examples \
 	--enable-realtime-only --enable-spatial-resampling \
-	--enable-vp8 --enable-multithread
+	--enable-vp8 --enable-multithread --disable-vp9
 
 ifneq (,$(findstring armv6,$(host)))
 	libvpx_configure_options+= --target=armv6-darwin-gcc --cpu=arm1176jzf-s
@@ -20,11 +20,17 @@ all_p+=armv7-darwin-gcc    #neon Cortex-A8
 all_p+=armv7s-darwin-gcc   #neon Cortex-A8
 
 
-$(BUILDER_BUILD_DIR)/$(libvpx_dir)/config.mk:
+$(BUILDER_SRC_DIR)/$(libvpx_dir)/patched.stamp:
+	cd $(BUILDER_SRC_DIR)/$(libvpx_dir) \
+	&& git apply $(BUILDER_SRC_DIR)/build/builders.d/libvpx.patch \
+	&& touch $@
+
+
+$(BUILDER_BUILD_DIR)/$(libvpx_dir)/config.mk: $(BUILDER_SRC_DIR)/$(libvpx_dir)/patched.stamp
 	mkdir -p $(BUILDER_BUILD_DIR)/$(libvpx_dir)
 	cd $(BUILDER_BUILD_DIR)/$(libvpx_dir)/ \
 	&&  host_alias=${host} . $(BUILDER_SRC_DIR)/build/$(config_site) \
-	&& export all_platforms="${all_p}" &&  $(BUILDER_SRC_DIR)/$(libvpx_dir)/configure --prefix=$(prefix) --sdk-path=$$SDK_BIN_PATH/../../ --libc=$$SYSROOT_PATH $(libvpx_configure_options) --extra-cflags="-O1 -fno-strict-aliasing"
+	&& export all_platforms="${all_p}" &&  $(BUILDER_SRC_DIR)/$(libvpx_dir)/configure --prefix=$(prefix) --sdk-path=$$SDK_BIN_PATH/../../ --libc=$$SYSROOT_PATH $(libvpx_configure_options) --extra-cflags="-fno-strict-aliasing"
 
 build-libvpx: $(BUILDER_BUILD_DIR)/$(libvpx_dir)/config.mk
 	cd $(BUILDER_BUILD_DIR)/$(libvpx_dir) \
@@ -37,7 +43,8 @@ clean-libvpx:
 veryclean-libvpx:
 	-cd $(BUILDER_BUILD_DIR)/$(libvpx_dir) && make distclean
 	cd $(BUILDER_SRC_DIR)/$(libvpx_dir) \
-	&& git clean -f && git reset --hard
+	&& git clean -f && git reset --hard \
+	&& rm -f patched.stamp 
 	rm -rf $(BUILDER_BUILD_DIR)/$(libvpx_dir)
 
 clean-makefile-libvpx:
