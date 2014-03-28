29cca9bfe976e9760084be00244ff9841209b0e9
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2014-03-28 14:12:07 +0100

    Use binary distribution for libvpx, since the new Apple compiler (Xcode 5.1) outputs crashing code.

--- submodules/build/builders.d/libvpx.mk
+++ submodules/build/builders.d/libvpx.mk
@@ -1,23 +1,29 @@
+libvpx_dir?=externals/libvpx
+
 libvpx_configure_options=\
 	--enable-static   --disable-shared \
 	--enable-error-concealment --disable-examples \
 	--enable-realtime-only --enable-spatial-resampling \
 	--enable-vp8 --enable-multithread --disable-vp9
 
+take_binary=
+
 ifneq (,$(findstring armv6,$(host)))
 	libvpx_configure_options+= --target=armv6-darwin-gcc --cpu=arm1176jzf-s
 else ifneq (,$(findstring armv7s,$(host)))
 	libvpx_configure_options+= --target=armv7s-darwin-gcc
+	take_binary = armv7s
 else ifneq (,$(findstring armv7,$(host)))
 	libvpx_configure_options+= --target=armv7-darwin-gcc
+	take_binary = armv7
 else
 	libvpx_configure_options+= --target=x86-darwin10-gcc
 endif
 
-libvpx_dir?=externals/libvpx
 all_p=armv6-darwin-gcc    #neon Cortex-A8
 all_p+=armv7-darwin-gcc    #neon Cortex-A8
 all_p+=armv7s-darwin-gcc   #neon Cortex-A8
+
 
 
 $(BUILDER_SRC_DIR)/$(libvpx_dir)/patched.stamp:
@@ -30,12 +36,17 @@ $(BUILDER_BUILD_DIR)/$(libvpx_dir)/config.mk: $(BUILDER_SRC_DIR)/$(libvpx_dir)/p
 	mkdir -p $(BUILDER_BUILD_DIR)/$(libvpx_dir)
 	cd $(BUILDER_BUILD_DIR)/$(libvpx_dir)/ \
 	&&  host_alias=${host} . $(BUILDER_SRC_DIR)/build/$(config_site) \
-	&& export all_platforms="${all_p}" &&  $(BUILDER_SRC_DIR)/$(libvpx_dir)/configure --prefix=$(prefix) --sdk-path=$$SDK_BIN_PATH/../../ --libc=$$SYSROOT_PATH $(libvpx_configure_options) --extra-cflags="-fno-strict-aliasing"
+	&& export all_platforms="${all_p}" &&  $(BUILDER_SRC_DIR)/$(libvpx_dir)/configure --prefix=$(prefix) --sdk-path=$$SDK_BIN_PATH/../../ --libc=$$SYSROOT_PATH $(libvpx_configure_options) --extra-cflags="-O1 -fno-strict-aliasing"
 
 build-libvpx: $(BUILDER_BUILD_DIR)/$(libvpx_dir)/config.mk
 	cd $(BUILDER_BUILD_DIR)/$(libvpx_dir) \
 	&& host_alias=${host} . $(BUILDER_SRC_DIR)/build/$(config_site) \
 	&& PKG_CONFIG_LIBDIR=$(prefix)/lib/pkgconfig CONFIG_SITE=$(BUILDER_SRC_DIR)/build/$(config_site) make && make install
+ifneq (,$(take_binary))
+# we have to take binary version of libvpx for ARM because Clang introduces bugs in optimized assembly
+	@echo "\033[01;32m Getting BINARY version of libvpx for $(take_binary) \033[0m"
+	cp $(BUILDER_SRC_DIR)/binaries/libvpx-$(take_binary).a $(prefix)/lib/libvpx.a
+endif
 
 clean-libvpx:
 	cd  $(BUILDER_BUILD_DIR)/$(libvpx_dir) && make clean
