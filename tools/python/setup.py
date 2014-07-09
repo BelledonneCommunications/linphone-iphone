4e487ade84db39a9f3fe3dec13bc08e77fac6e61
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-07-09 10:21:16 +0200

    Link against liblinphone.

--- tools/python/setup.py
+++ tools/python/setup.py
@@ -4,6 +4,8 @@ from distutils.core import setup, Extension
 
 m = Extension('linphone',
 	include_dirs = ['/home/ghislain/linphone-install/include'],
+	libraries = ['linphone'],
+	library_dirs = ['/home/ghislain/linphone-install/lib'],
 	sources = ['linphone.c']
 )
 setup(name = 'Linphone', version = '1.0', description = 'Linphone package', ext_modules = [m])
