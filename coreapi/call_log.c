b6d1b58b1778b4945f1af72d6c7293178d3fccfe
François Grisez <francois.grisez@belledonne-communications.com>
2014-08-29 16:32:27 +0200

    Fix compilation error. Add missing #define _GNU_SOURCE

--- coreapi/call_log.c
+++ coreapi/call_log.c
@@ -17,9 +17,13 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+#define _GNU_SOURCE
 
 #include "linphonecore.h"
 #include "private.h"
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 
 /*******************************************************************************
