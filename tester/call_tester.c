67658d5eb588462c787e7c082f818de26289a822
Simon Morlat <simon.morlat@linphone.org>
2014-09-25 14:31:39 +0200

    add new play test

--- tester/call_tester.c
+++ tester/call_tester.c
@@ -2975,7 +2975,6 @@ static void record_call(const char *filename, bool_t enableVideo) {
 			linphone_call_stop_recording(callInst);
 			end_call(marie, pauline);
 			CU_ASSERT_EQUAL(access(filepath, F_OK), 0);
-			remove(filepath);
 		}
 		ms_free(filepath);
 	}
