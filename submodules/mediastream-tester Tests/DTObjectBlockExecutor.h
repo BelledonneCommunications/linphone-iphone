f564901ccd9fb0953b2bec8f61eb8e76543df8b3
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2014-11-21 10:43:48 +0100

    Add media streamer unit tests for Xcode, this allows to launch it from the command line

---
+++ submodules/mediastream-tester Tests/DTObjectBlockExecutor.h
@@ -0,0 +1,28 @@
+//
+//  DTObjectBlockExecutor.h
+//  DTFoundation
+//
+//  Created by Oliver Drobnik on 12.02.13.
+//  Copyright (c) 2013 Cocoanetics. All rights reserved.
+//
+
+/**
+ This class is used by [NSObject addDeallocBlock:] to execute blocks on dealloc
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface DTObjectBlockExecutor : NSObject
+
+/**
+ Convenience method to create a block executor with a deallocation block
+ @param block The block to execute when the created receiver is being deallocated
+ */
++ (id)blockExecutorWithDeallocBlock:(void(^)())block;
+
+/**
+ Block to execute when dealloc of the receiver is called
+ */
+@property (nonatomic, copy) void (^deallocBlock)();
+
+@end
