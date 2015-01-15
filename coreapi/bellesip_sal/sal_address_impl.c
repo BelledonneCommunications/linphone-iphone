838520350c0a96d5730d745db06e5cc886a61c25
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2015-01-15 17:19:58 +0100

    Use const for sal_address_is_ipv6()

--- coreapi/bellesip_sal/sal_address_impl.c
+++ coreapi/bellesip_sal/sal_address_impl.c
@@ -215,7 +215,7 @@ void sal_address_unref(SalAddress *addr){
 	belle_sip_object_unref(BELLE_SIP_HEADER_ADDRESS(addr));
 }
 
-bool_t sal_address_is_ipv6(SalAddress *addr){
+bool_t sal_address_is_ipv6(const SalAddress *addr){
 	belle_sip_header_address_t* header_addr = BELLE_SIP_HEADER_ADDRESS(addr);
 	belle_sip_uri_t* uri = belle_sip_header_address_get_uri(header_addr);
 	if (uri){
--- include/sal/sal.h
+++ include/sal/sal.h
@@ -109,7 +109,7 @@ void sal_address_set_transport(SalAddress* addr,SalTransport transport);
 void sal_address_set_transport_name(SalAddress* addr,const char* transport);
 void sal_address_set_params(SalAddress *addr, const char *params);
 void sal_address_set_uri_params(SalAddress *addr, const char *params);
-bool_t sal_address_is_ipv6(SalAddress *addr);
+bool_t sal_address_is_ipv6(const SalAddress *addr);
 bool_t sal_address_is_sip(const SalAddress *addr);
 void sal_address_set_password(SalAddress *addr, const char *passwd);
 const char *sal_address_get_password(const SalAddress *addr);
