f953b328eab05d15164e077a54d3cbc3cceb973e
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2015-02-27 15:10:30 +0100

    Revert "gsm builder: Rewrite veryclean target and correctly build targetted architecture"
    This reverts commit 5e60c1864eab4ec35ea4ddd26aa21634a0289616.
    
    Conflicts:
    	submodules/build/builders.d/gsm.mk

--- submodules/build/builders.d/gsm.mk
+++ submodules/build/builders.d/gsm.mk
@@ -3,9 +3,7 @@
 
 gsm_dir?=externals/gsm
 
-#GSM build is a bit different: since there is only a Makefile,
-#we must force CC to contains CFLAGS to compile all architectures
-#as expected
+
 build-libgsm:
 	cp -rf $(BUILDER_SRC_DIR)/$(gsm_dir) $(BUILDER_BUILD_DIR)/$(gsm_dir)
 	rm -rf $(BUILDER_BUILD_DIR)/$(gsm_dir)/gsm/.git $(BUILDER_BUILD_DIR)/$(gsm_dir)/.git
@@ -14,13 +12,12 @@ build-libgsm:
 	cd $(BUILDER_BUILD_DIR)/$(gsm_dir)\
 	&& mkdir -p $(prefix)/include/gsm \
 	&& host_alias=$(host)  . $(BUILDER_SRC_DIR)/build/$(config_site) \
-	&&  make -j1 CC="$${CC}" INSTALL_ROOT=$(prefix)  GSM_INSTALL_INC=$(prefix)/include/gsm  install
+	&& make -j1 CC="$${CC}" INSTALL_ROOT=$(prefix)  GSM_INSTALL_INC=$(prefix)/include/gsm  install
 
 clean-libgsm:
 	cd $(BUILDER_BUILD_DIR)/$(gsm_dir)\
 	&& make clean
 
 veryclean-libgsm:
-	-rm -rf $(BUILD_DIR)/$(gsm_dir)
-
-
+	 -cd $(BUILDER_BUILD_DIR)/$(gsm_dir) \
+	&& make uninstall
