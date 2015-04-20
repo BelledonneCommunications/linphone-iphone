b6d7b48d2ec084f94f0e8e57b6c46efa8a4534c9
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2015-04-20 11:50:05 +0200

    i10n: add Settings page to translatable resources

--- Tools/generate_strings_files.sh
+++ Tools/generate_strings_files.sh
@@ -4,47 +4,86 @@ root_directory=$(cd "$(dirname $0)" && pwd)/../
 
 set -e
 
-# The 2 only specific cases of the application: since we are length limited for push
-# notifications, the ID is not matching the English translation... so we must keep
-# the translations!
-localizable_en=$root_directory/Resources/en.lproj/Localizable.strings
-IC_MSG_EN=$(sed -nE 's/"IC_MSG" = "(.*)";/\1/p' $localizable_en)
-IM_MSG_EN=$(sed -nE 's/"IM_MSG" = "(.*)";/\1/p' $localizable_en)
-rm -f $localizable_en
-find $root_directory/Classes -name '*.m' | xargs genstrings -u -a -o $(dirname $localizable_en)
-iconv -f utf-16 -t utf-8 $localizable_en > $localizable_en.tmp
-mv $localizable_en.tmp $localizable_en
-sed -i.bak "s/= \"IC_MSG\";/= \"$IC_MSG_EN\";/" $localizable_en
-sed -i.bak "s/= \"IM_MSG\";/= \"$IM_MSG_EN\";/" $localizable_en
-rm $localizable_en.bak
+if [ $# != 0 ]; then
+	echo "No argument needed. This script will (re)generate .strings file from .xib files and register them in the transifex config file located in .tx/config."
+	exit 0
+fi
 
-to_utf8=$(mktemp -t linphone)
+##### 1. Generate Localizable.strings from source files (.m)
+function generate_localizable_from_sources {
+	localizable_en=$root_directory/Resources/en.lproj/Localizable.strings
+	# The 2 only specific cases of the application: since we are length limited for push
+	# notifications, the ID is not matching the English translation... so we must keep
+	# the translations!
+	IC_MSG_EN=$(sed -nE 's/"IC_MSG" = "(.*)";/\1/p' $localizable_en)
+	IM_MSG_EN=$(sed -nE 's/"IM_MSG" = "(.*)";/\1/p' $localizable_en)
+	rm -f $localizable_en
+	find $root_directory/Classes -name '*.m' | xargs genstrings -u -a -o $(dirname $localizable_en)
+	iconv -f utf-16 -t utf-8 $localizable_en > $localizable_en.tmp
+	mv $localizable_en.tmp $localizable_en
+	sed -i.bak "s/= \"IC_MSG\";/= \"$IC_MSG_EN\";/" $localizable_en
+	sed -i.bak "s/= \"IM_MSG\";/= \"$IM_MSG_EN\";/" $localizable_en
+	rm $localizable_en.bak
+}
 
-find $root_directory/Classes -not -path "$root_directory/Classes/KIF/*" -name Base.lproj -exec find {} -name '*.xib' \; | while read -r xibfile; do
-	stringsfile="${xibfile/.xib/.strings}"
+##### 2. Generate .strings for all XIB files
+function generate_strings_from_xib {
+	to_utf8_file=$(mktemp -t linphone)
+	find $root_directory/Classes -not -path "$root_directory/Classes/KIF/*" -name Base.lproj -exec find {} -name '*.xib' \; | while read -r xibfile; do
+		stringsfile="${xibfile/.xib/.strings}"
 
-	ibtool --generate-strings-file "$stringsfile" "$xibfile"
+		ibtool --generate-strings-file "$stringsfile" "$xibfile"
 
-	# remove if empty
-	iconv -f utf-16 -t utf-8 "$stringsfile" > "$to_utf8"
-	if [ ! -s "$to_utf8" ]; then
-		echo "$(basename "$stringsfile") is empty, removing"
-		rm "$stringsfile"
-	else
-		echo "$(basename "$xibfile")->$(basename "$stringsfile")"
+		# remove if empty
+		iconv -f utf-16 -t utf-8 "$stringsfile" > "$to_utf8_file"
+		if [ ! -s "$to_utf8_file" ]; then
+			echo "$(basename "$stringsfile") is empty, removing"
+			rm "$stringsfile"
+		else
+			echo "$(basename "$xibfile")->$(basename "$stringsfile")"
 
-		res_name=$(basename "$stringsfile" | tr -d '_.~-' | tr '[:upper:]' '[:lower:]')
-		dir_name=$(echo $(dirname "$stringsfile") | sed -E "s|$root_directory/||")
-		# if not registered in transifex config file, register it
+			res_name=$(basename "$stringsfile" | tr -d '_.~-' | tr '[:upper:]' '[:lower:]')
+			dir_name=$(echo $(dirname "$stringsfile") | sed -E "s|$root_directory/||")
+			# if not registered in transifex config file, register it
+			if ! grep -q $res_name $root_directory/.tx/config; then
+				echo "not found in .tx/config, adding it"
+				echo "
+	[linphone-ios.$res_name]
+	file_filter = $(echo $dir_name| sed 's/Base.lproj/<lang>.lproj/')/$(basename "$stringsfile")
+	source_file = $dir_name/$(basename "$stringsfile")
+	source_lang = en
+	" >> $root_directory/.tx/config
+			fi
+		fi
+	done
+	rm $to_utf8_file
+}
+
+##### 3. Generate .strings for all InAppSettings PLIST files
+function generate_strings_from_inappsettings_plist {
+	tmp_file=$(mktemp -t linphone)
+	find $root_directory/Settings/InAppSettings.bundle -name '*.plist' | while read -r plistfile; do
+		echo $plistfile
+		plistfilestrings="$(basename ${plistfile/.plist/.strings})"
+		printf '' > $tmp_file
+		while read title; do
+			echo "\"$title\" = \"$title\";" >> $tmp_file
+		done <<< "$(grep -e '>Title<' $plistfile -A 1 | sed -nE 's|.*<string>(.*)</string>.*|\1|p')"
+
+		mv $tmp_file $root_directory/Settings/InAppSettings.bundle/en.lproj/$plistfilestrings
+
+		res_name=$(echo "$plistfilestrings" | tr -d '_.~-' | tr '[:upper:]' '[:lower:]')
 		if ! grep -q $res_name $root_directory/.tx/config; then
 			echo "not found in .tx/config, adding it"
 			echo "
 [linphone-ios.$res_name]
-file_filter = $(echo $dir_name| sed 's/Base.lproj/<lang>.lproj/')/$(basename "$stringsfile")
-source_file = $dir_name/$(basename "$stringsfile")
+file_filter = Settings/InAppSettings.bundle/<lang>.lproj/$plistfilestrings
 source_lang = en
 " >> $root_directory/.tx/config
 		fi
-	fi
-done
-rm $to_utf8
+	done
+}
+
+# generate_localizable_from_sources
+# generate_strings_from_xib
+generate_strings_from_inappsettings_plist
