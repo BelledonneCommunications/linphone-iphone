29d909de8e78712ebd7092181096c0529acbc09c
François Grisez <francois.grisez@belledonne-communications.com>
2015-06-26 15:44:52 +0200

    Add 'Arabic' and 'Turkish' in the language selection list

--- gtk/propertybox.c
+++ gtk/propertybox.c
@@ -1106,6 +1106,8 @@ static LangCodes supported_langs[]={
 	{	"nb_NO"	,	N_("Norwegian")	},
 	{	"he"	,	N_("Hebrew")	},
 	{	"sr"	,	N_("Serbian")	},
+	{	"ar"	,	N_("Arabic")	},
+	{	"tr"	,	N_("Turkish")	},
 	{	NULL	,	NULL		}
 };
 
