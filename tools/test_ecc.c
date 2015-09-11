d4c76523389a3921817bcdb5c2a65385ef37ae5f
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2015-09-11 10:09:10 +0200

    tools: move test_lsd test_ecc test_numbers from coreapi to tools folder

--- tools/test_ecc.c
+++ tools/test_ecc.c
@@ -18,47 +18,46 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-
 #include "linphonecore.h"
 #include "linphonecore_utils.h"
 #if _MSC_VER
 #include <io.h>
 #endif
 
-static void calibration_finished(LinphoneCore *lc, LinphoneEcCalibratorStatus status, int delay, void *data){
-	ms_message("echo calibration finished %s.",status==LinphoneEcCalibratorDone ? "successfully" : "with faillure");
-	if (status==LinphoneEcCalibratorDone) ms_message("Measured delay is %i",delay);
+static void calibration_finished(LinphoneCore *lc, LinphoneEcCalibratorStatus status, int delay, void *data) {
+	ms_message("echo calibration finished %s.", status == LinphoneEcCalibratorDone ? "successfully" : "with faillure");
+	if (status == LinphoneEcCalibratorDone)
+		ms_message("Measured delay is %i", delay);
 }
 
-
 static char config_file[1024];
-void parse_args(int argc, char *argv[]){
+void parse_args(int argc, char *argv[]) {
 #ifndef F_OK
 #define F_OK 4
 #endif
-	if (argc != 3 || strncmp("-c",argv[1], 2) || access(argv[2],F_OK)!=0) {
+	if (argc != 3 || strncmp("-c", argv[1], 2) || access(argv[2], F_OK) != 0) {
 		printf("Usage: test_ecc [-c config_file] where config_file will be written with the detected value\n");
 		exit(-1);
 	}
-	strncpy(config_file,argv[2],1024);
+	strncpy(config_file, argv[2], 1024);
 }
 
-int main(int argc, char *argv[]){
-	int count=0;
-	LinphoneCoreVTable vtable={0};
+int main(int argc, char *argv[]) {
+	int count = 0;
+	LinphoneCoreVTable vtable = {0};
 	LinphoneCore *lc;
-	if (argc>1) parse_args(argc,argv);
-	lc=linphone_core_new(&vtable,config_file,NULL,NULL);
-	
+	if (argc > 1)
+		parse_args(argc, argv);
+	lc = linphone_core_new(&vtable, config_file, NULL, NULL);
+
 	linphone_core_enable_logs(NULL);
 
-	linphone_core_start_echo_calibration(lc,calibration_finished,NULL,NULL,NULL);
-	
-	while(count++<1000){
+	linphone_core_start_echo_calibration(lc, calibration_finished, NULL, NULL, NULL);
+
+	while (count++ < 1000) {
 		linphone_core_iterate(lc);
 		ms_usleep(10000);
 	}
 	linphone_core_destroy(lc);
 	return 0;
 }
-
