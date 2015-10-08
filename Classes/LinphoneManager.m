dfdc51e817abf1d430142674020f38ef0217c754
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2015-10-08 15:19:29 +0200

    assistant: use TLS transport for quality reporting

--- Classes/LinphoneManager.m
+++ Classes/LinphoneManager.m
@@ -523,7 +523,8 @@ exit_dbmigration:
 			// we want to enable quality reporting for the proxies that are on linphone.org
 			if (addr && strstr(addr, "sip.linphone.org") != 0) {
 				LOGI(@"Migrating proxy config to send quality report");
-				linphone_proxy_config_set_quality_reporting_collector(proxy, "sip:voip-metrics@sip.linphone.org");
+				linphone_proxy_config_set_quality_reporting_collector(
+					proxy, "sip:voip-metrics@sip.linphone.org;transport=tls");
 				linphone_proxy_config_set_quality_reporting_interval(proxy, 180);
 				linphone_proxy_config_enable_quality_reporting(proxy, TRUE);
 			}
