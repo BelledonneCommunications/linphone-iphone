b66d57d5b5cba9d5e98c1dced00a489dd677923e
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2015-10-15 16:30:15 +0200

    submodules: update linphone

--- Classes/IncomingCallViewController.m
+++ Classes/IncomingCallViewController.m
@@ -93,7 +93,7 @@ static UICompositeViewDescription *compositeDescription = nil;
 		[self dismiss];
 	} else if ([LinphoneManager.instance lpConfigBoolForKey:@"auto_answer"]) {
 		LinphoneCallState state = linphone_call_get_state(call);
-		if (state == LinphoneCallIncomingReceived || state == LinphoneCallIncomingEarlyMedia) {
+		if (state == LinphoneCallIncomingReceived) {
 			[self onAcceptClick:nil];
 		}
 	}
