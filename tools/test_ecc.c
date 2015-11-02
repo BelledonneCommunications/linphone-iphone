bd00a4df59747b6df473e79befc0f546b33abd19
Simon Morlat <simon.morlat@linphone.org>
2015-11-02 21:53:46 +0100

    improve echo calibration user experience.

--- tools/test_ecc.c
+++ tools/test_ecc.c
@@ -23,14 +23,16 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #if _MSC_VER
 #include <io.h>
 #endif
+static int done = 0;
 
 static void calibration_finished(LinphoneCore *lc, LinphoneEcCalibratorStatus status, int delay, void *data) {
 	ms_message("echo calibration finished %s.", status == LinphoneEcCalibratorDone ? "successfully" : "with faillure");
 	if (status == LinphoneEcCalibratorDone)
 		ms_message("Measured delay is %i", delay);
+	done = 1;
 }
 
-static char config_file[1024];
+static char config_file[1024] = {0};
 void parse_args(int argc, char *argv[]) {
 #ifndef F_OK
 #define F_OK 4
@@ -43,20 +45,19 @@ void parse_args(int argc, char *argv[]) {
 }
 
 int main(int argc, char *argv[]) {
-	int count = 0;
 	LinphoneCoreVTable vtable = {0};
 	LinphoneCore *lc;
 	if (argc > 1)
 		parse_args(argc, argv);
-	lc = linphone_core_new(&vtable, config_file, NULL, NULL);
+	lc = linphone_core_new(&vtable, config_file[0] ?  config_file  : NULL, NULL, NULL);
 
 	linphone_core_enable_logs(NULL);
 
 	linphone_core_start_echo_calibration(lc, calibration_finished, NULL, NULL, NULL);
 
-	while (count++ < 1000) {
+	while (!done) {
 		linphone_core_iterate(lc);
-		ms_usleep(10000);
+		ms_usleep(20000);
 	}
 	linphone_core_destroy(lc);
 	return 0;
