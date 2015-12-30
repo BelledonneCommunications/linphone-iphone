8cc1a6d6f081255986cbdff8bbfed0801953a809
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2015-12-30 17:31:04 +0100

    Fix windows compilation when belcard isn't found/enabled

--- coreapi/vcard_stubs.c
+++ coreapi/vcard_stubs.c
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "vcard.h"
 
 struct _LinphoneVCard {
+	void *dummy;
 };
 
 LinphoneVCard* linphone_vcard_new(void) {
