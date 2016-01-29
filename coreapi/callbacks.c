580310fc74c6d1b1f986d68a71d2a690c88a1500
Jehan Monnier <jehan.monnier@linphone.org>
2016-01-29 14:00:24 +0100

    implement io error handling for list subscription

--- coreapi/callbacks.c
+++ coreapi/callbacks.c
@@ -1303,7 +1303,7 @@ static void subscribe_response(SalOp *op, SalSubscribeStatus status){
 	}else if (status==SalSubscribePending){
 		linphone_event_set_state(lev,LinphoneSubscriptionPending);
 	}else{
-		if (lev->subscription_state==LinphoneSubscriptionActive && ei->reason==SalReasonIOError){
+		if (lev->subscription_state==LinphoneSubscriptionActive && (ei->reason==SalReasonIOError || ei->reason == SalReasonNoMatch)){
 			linphone_event_set_state(lev,LinphoneSubscriptionOutgoingProgress);
 		}
 		else linphone_event_set_state(lev,LinphoneSubscriptionError);
