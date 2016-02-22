17ee1f15902825fd5e8b31693c54dd6f7c0b98ff
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2016-02-22 16:25:23 +0100

    chat.c: dont change message state to in progress if we failed to send it

--- coreapi/chat.c
+++ coreapi/chat.c
@@ -418,7 +418,10 @@ void _linphone_chat_room_send_message(LinphoneChatRoom *cr, LinphoneChatMessage 
 		linphone_chat_room_delete_composing_refresh_timer(cr);
 
 	}
-	linphone_chat_message_set_state(msg, LinphoneChatMessageStateInProgress);
+	// if operation failed, we should not change message state
+	if (msg->state == LinphoneChatMessageOutgoing) {
+		linphone_chat_message_set_state(msg, LinphoneChatMessageStateInProgress);
+	}
 }
 
 void linphone_chat_message_update_state(LinphoneChatMessage *msg, LinphoneChatMessageState new_state) {
