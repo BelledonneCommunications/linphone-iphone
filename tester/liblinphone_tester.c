82106e85a2fb142bdcc91a9b530257bc9aebd199
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2016-06-03 10:33:15 +0200

    liblinphone_tester.c: properly handle --6 option

--- tester/liblinphone_tester.c
+++ tester/liblinphone_tester.c
@@ -191,7 +191,9 @@ static const char* liblinphone_helper =
 		"\t\t\t--auth-domain <test auth domain>\n"
 		"\t\t\t--dns-hosts </etc/hosts -like file to used to override DNS names (default: tester_hosts)>\n"
 		"\t\t\t--keep-recorded-files\n"
-		"\t\t\t--disable-leak-detector\n";
+		"\t\t\t--disable-leak-detector\n"
+		"\t\t\t--6\n"
+		;
 
 int main (int argc, char *argv[])
 {
