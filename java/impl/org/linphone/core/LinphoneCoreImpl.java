c979886e1f6daf89699aefea714aab5a99c0ee16
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2016-07-08 16:43:32 +0200

    Add NAT policy JNI wrapper.

--- java/impl/org/linphone/core/LinphoneCoreImpl.java
+++ java/impl/org/linphone/core/LinphoneCoreImpl.java
@@ -111,6 +111,9 @@ class LinphoneCoreImpl implements LinphoneCore {
 	private native boolean isVideoSupported(long nativePtr);
 	private native void setFirewallPolicy(long nativePtr, int enum_value);
 	private native int getFirewallPolicy(long nativePtr);
+	private native Object createNatPolicy(long nativePtr);
+	private native void setNatPolicy(long nativePtr, long policyPtr);
+	private native Object getNatPolicy(long nativePtr);
 	private native void setStunServer(long nativePtr, String stun_server);
 	private native String getStunServer(long nativePtr);
 	private native int updateCall(long ptrLc, long ptrCall, long ptrParams);
@@ -524,11 +527,20 @@ class LinphoneCoreImpl implements LinphoneCore {
 	public synchronized FirewallPolicy getFirewallPolicy() {
 		return FirewallPolicy.fromInt(getFirewallPolicy(nativePtr));
 	}
-	public synchronized String getStunServer() {
-		return getStunServer(nativePtr);
-	}
 	public synchronized void setFirewallPolicy(FirewallPolicy pol) {
 		setFirewallPolicy(nativePtr,pol.value());
+	}
+	public synchronized LinphoneNatPolicy createNatPolicy() {
+		return (LinphoneNatPolicy)createNatPolicy(nativePtr);
+	}
+	public synchronized void setNatPolicy(LinphoneNatPolicy policy) {
+		setNatPolicy(nativePtr, ((LinphoneNatPolicyImpl)policy).mNativePtr);
+	}
+	public synchronized LinphoneNatPolicy getNatPolicy() {
+		return (LinphoneNatPolicy)getNatPolicy(nativePtr);
+	}
+	public synchronized String getStunServer() {
+		return getStunServer(nativePtr);
 	}
 	public synchronized void setStunServer(String stunServer) {
 		setStunServer(nativePtr, stunServer);
