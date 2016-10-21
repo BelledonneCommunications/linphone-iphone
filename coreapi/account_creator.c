7a2470c33d69e1a6e5999481175ada66015b61af
Simon Morlat <simon.morlat@linphone.org>
2016-10-21 14:09:03 +0200

    fix bug

--- coreapi/account_creator.c
+++ coreapi/account_creator.c
@@ -476,9 +476,9 @@ LinphoneProxyConfig * linphone_account_creator_configure(const LinphoneAccountCr
 		snprintf(buff, sizeof(buff), "%d", dial_prefix_number);
 		linphone_proxy_config_set_dial_prefix(cfg, buff);
 	}
-	if(linphone_proxy_config_get_server_addr == NULL)
+	if (linphone_proxy_config_get_server_addr(cfg) == NULL)
 		linphone_proxy_config_set_server_addr(cfg, domain);
-	if(linphone_proxy_config_get_route == NULL)	
+	if (linphone_proxy_config_get_route(cfg) == NULL)
 		linphone_proxy_config_set_route(cfg, route);
 	linphone_proxy_config_enable_publish(cfg, FALSE);
 	linphone_proxy_config_enable_register(cfg, TRUE);
