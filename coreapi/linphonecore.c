0435a0219767f14a058f1cbbee72cf01b1936794
Sandrine Avakian <sandrine.avakian@belledonne-communications.com>
2016-12-26 15:33:16 +0100

    Adding option in linphonerc rtp_io_enable_local_output to enable using local speaker in rtp_io mode. Option checked in [sound] section only.

--- coreapi/linphonecore.c
+++ coreapi/linphonecore.c
@@ -6937,6 +6937,7 @@ void linphone_core_soundcard_hint_check( LinphoneCore* lc){
 	LinphoneCall* call = NULL;
 	bool_t dont_need_sound = TRUE;
 	bool_t use_rtp_io = lp_config_get_int(lc->config, "sound", "rtp_io", FALSE);
+	bool_t use_rtp_io_enable_local_output = lp_config_get_int(lc->config, "sound", "rtp_io_enable_local_output", FALSE);
 
 	/* check if the remaining calls are paused */
 	while( the_calls ){
@@ -6949,7 +6950,7 @@ void linphone_core_soundcard_hint_check( LinphoneCore* lc){
 	}
 
 	/* if no more calls or all calls are paused, we can free the soundcard */
-	if ( (lc->calls==NULL || dont_need_sound) && !lc->use_files && !use_rtp_io){
+	if ( (lc->calls==NULL || dont_need_sound) && !lc->use_files && (!use_rtp_io || (use_rtp_io && use_rtp_io_enable_local_output))){
 		ms_message("Notifying soundcard that we don't need it anymore for calls.");
 		notify_soundcard_usage(lc,FALSE);
 	}
