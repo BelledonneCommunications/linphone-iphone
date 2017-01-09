576788be5c936d69fac612661d7916dc7b2baac7
Benjamin Reis <benjamin.reis@belledonne-communications.com>
2017-01-09 17:33:14 +0100

    Repair repeat call notif when auto-answer

--- Classes/LinphoneAppDelegate.m
+++ Classes/LinphoneAppDelegate.m
@@ -112,6 +112,13 @@
 		} else if (linphone_call_get_state(call) == LinphoneCallIncomingReceived) {
 			if ((floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max)) {
 				if ([LinphoneManager.instance lpConfigBoolForKey:@"autoanswer_notif_preference"]) {
+					if (call) {
+						LinphoneCallAppData *data = (__bridge LinphoneCallAppData *)linphone_call_get_user_data(call);
+						if (data->timer) {
+							[data->timer invalidate];
+							data->timer = nil;
+						}
+					}
 					linphone_core_accept_call(LC, call);
 					[PhoneMainView.instance changeCurrentView:CallView.compositeViewDescription];
 				} else {
