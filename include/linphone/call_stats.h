558659155ce833ebd583fd992043ad765afb796e
François Grisez <francois.grisez@belledonne-communications.com>
2017-01-20 11:08:31 +0100

    Reformat the LinphoneCallStats structure

--- include/linphone/call_stats.h
+++ include/linphone/call_stats.h
@@ -52,23 +52,23 @@ typedef struct _LinphoneCallStats LinphoneCallStats;
  * At any time, the application can access last computed statistics using linphone_call_get_audio_stats() or linphone_call_get_video_stats().
 **/
 struct _LinphoneCallStats {
-	LinphoneStreamType		type; /**< Type of the stream which the stats refer to */
-	jitter_stats_t	jitter_stats; /**<jitter buffer statistics, see oRTP documentation for details */
-	mblk_t*		received_rtcp; /**<Last RTCP packet received, as a mblk_t structure. See oRTP documentation for details how to extract information from it*/
-	mblk_t*		sent_rtcp;/**<Last RTCP packet sent, as a mblk_t structure. See oRTP documentation for details how to extract information from it*/
-	float		round_trip_delay; /**<Round trip propagation time in seconds if known, -1 if unknown.*/
-	LinphoneIceState	ice_state; /**< State of ICE processing. */
-	LinphoneUpnpState	upnp_state; /**< State of uPnP processing. */
-	float download_bandwidth; /**<Download bandwidth measurement of received stream, expressed in kbit/s, including IP/UDP/RTP headers*/
-	float upload_bandwidth; /**<Download bandwidth measurement of sent stream, expressed in kbit/s, including IP/UDP/RTP headers*/
-	float local_late_rate; /**<percentage of packet received too late over last second*/
-	float local_loss_rate; /**<percentage of lost packet over last second*/
-	int updated; /**< Tell which RTCP packet has been updated (received_rtcp or sent_rtcp). Can be either LINPHONE_CALL_STATS_RECEIVED_RTCP_UPDATE or LINPHONE_CALL_STATS_SENT_RTCP_UPDATE */
-	float rtcp_download_bandwidth; /**<RTCP download bandwidth measurement of received stream, expressed in kbit/s, including IP/UDP/RTP headers*/
-	float rtcp_upload_bandwidth; /**<RTCP download bandwidth measurement of sent stream, expressed in kbit/s, including IP/UDP/RTP headers*/
-	rtp_stats_t rtp_stats; /**< RTP stats */
-	bool_t rtcp_received_via_mux; /*private flag, for non-regression test only*/
-	int rtp_remote_family; /* Ip adress family of the remote destination */
+    LinphoneStreamType type; /**< Type of the stream which the stats refer to */
+    jitter_stats_t jitter_stats; /**<jitter buffer statistics, see oRTP documentation for details */
+    mblk_t *received_rtcp; /**<Last RTCP packet received, as a mblk_t structure. See oRTP documentation for details how to extract information from it*/
+    mblk_t *sent_rtcp;/**<Last RTCP packet sent, as a mblk_t structure. See oRTP documentation for details how to extract information from it*/
+    float round_trip_delay; /**<Round trip propagation time in seconds if known, -1 if unknown.*/
+    LinphoneIceState ice_state; /**< State of ICE processing. */
+    LinphoneUpnpState	upnp_state; /**< State of uPnP processing. */
+    float download_bandwidth; /**<Download bandwidth measurement of received stream, expressed in kbit/s, including IP/UDP/RTP headers*/
+    float upload_bandwidth; /**<Download bandwidth measurement of sent stream, expressed in kbit/s, including IP/UDP/RTP headers*/
+    float local_late_rate; /**<percentage of packet received too late over last second*/
+    float local_loss_rate; /**<percentage of lost packet over last second*/
+    int updated; /**< Tell which RTCP packet has been updated (received_rtcp or sent_rtcp). Can be either LINPHONE_CALL_STATS_RECEIVED_RTCP_UPDATE or LINPHONE_CALL_STATS_SENT_RTCP_UPDATE */
+    float rtcp_download_bandwidth; /**<RTCP download bandwidth measurement of received stream, expressed in kbit/s, including IP/UDP/RTP headers*/
+    float rtcp_upload_bandwidth; /**<RTCP download bandwidth measurement of sent stream, expressed in kbit/s, including IP/UDP/RTP headers*/
+    rtp_stats_t rtp_stats; /**< RTP stats */
+    bool_t rtcp_received_via_mux; /*private flag, for non-regression test only*/
+    int rtp_remote_family; /* Ip adress family of the remote destination */
 };
 
 /**
