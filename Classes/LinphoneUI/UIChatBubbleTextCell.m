cfaac82b82fb244d83fc48ddbbc567e6ff95f7d2
Brieuc Viel <brieuc.viel@belledonne-communications.com>
2017-03-21 16:02:06 +0100

    [imdm] updates UI text and space

--- Classes/LinphoneUI/UIChatBubbleTextCell.m
+++ Classes/LinphoneUI/UIChatBubbleTextCell.m
@@ -262,6 +262,7 @@ static void message_status(LinphoneChatMessage *msg, LinphoneChatMessageState st
 		[_imdmLabel setText:NSLocalizedString(@"Displayed", nil)];
 		[_imdmLabel
 			setTextColor:([UIColor colorWithRed:(24 / 255.0) green:(167 / 255.0) blue:(175 / 255.0) alpha:1.0])];
+
 		[_imdmIcon setHidden:FALSE];
 		[_imdmLabel setHidden:FALSE];
 	} else if (state == LinphoneChatMessageStateNotDelivered || state == LinphoneChatMessageStateFileTransferError) {
