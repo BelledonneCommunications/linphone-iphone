4abf8a51c1db01d4f329c1730f35dd88b3307d28
Jehan Monnier <jehan.monnier@linphone.org>
2017-03-28 15:57:09 +0200

    update belle-sip & linphone subs

--- Classes/ContactsListView.m
+++ Classes/ContactsListView.m
@@ -121,7 +121,6 @@ static UICompositeViewDescription *compositeDescription = nil;
 - (void)viewWillAppear:(BOOL)animated {
 	[super viewWillAppear:animated];
 	[ContactSelection setNameOrEmailFilter:@""];
-	[tableController loadData];
 	_searchBar.showsCancelButton = (_searchBar.text.length > 0);
 
 	if (tableController.isEditing) {
