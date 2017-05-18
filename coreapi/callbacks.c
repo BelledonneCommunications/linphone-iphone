84441343adedad0007a05b438abe04a7c105c6ca
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2017-05-18 15:11:17 +0200

    Fix crash because of unref of event.

--- coreapi/callbacks.c
+++ coreapi/callbacks.c
@@ -1138,7 +1138,6 @@ static void register_failure(SalOp *op){
 	if (cfg->long_term_event){
 		/*prevent publish to be sent now until registration gets successful*/
 		linphone_event_terminate(cfg->long_term_event);
-		linphone_event_unref(cfg->long_term_event);
 		cfg->long_term_event=NULL;
 		cfg->send_publish=cfg->publish;
 	}
