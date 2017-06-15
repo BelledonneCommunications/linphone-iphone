0e5f5378b5e1ac8e9300fced1279550e972ebd87
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2017-06-15 17:03:54 +0200

    Add missing symbol export.

--- include/sal/sal.h
+++ include/sal/sal.h
@@ -661,7 +661,7 @@ const char *sal_get_root_ca(Sal* ctx);
 void sal_verify_server_certificates(Sal *ctx, bool_t verify);
 void sal_verify_server_cn(Sal *ctx, bool_t verify);
 void sal_set_ssl_config(Sal *ctx, void *ssl_config);
-void sal_set_uuid(Sal*ctx, const char *uuid);
+LINPHONE_PUBLIC void sal_set_uuid(Sal*ctx, const char *uuid);
 int sal_create_uuid(Sal*ctx, char *uuid, size_t len);
 int sal_generate_uuid(char *uuid, size_t len);
 LINPHONE_PUBLIC void sal_enable_test_features(Sal*ctx, bool_t enabled);
