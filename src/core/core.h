98a370e79fa8143fcd9dc807964f870c0eb65db4
Ronan Abhamon <ronan.abhamon@belledonne-communications.com>
2017-08-24 16:01:39 +0200

    feat(core): add empty `Core` class

---
+++ src/core/core.h
@@ -0,0 +1,43 @@
+/*
+ * core.h
+ * Copyright (C) 2017  Belledonne Communications SARL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _CORE_H_
+#define _CORE_H_
+
+#include "object/object.h"
+
+// =============================================================================
+
+LINPHONE_BEGIN_NAMESPACE
+
+class CorePrivate;
+
+class LINPHONE_PUBLIC Core : public Object {
+public:
+	// Nothing for the moment.
+
+private:
+	Core (CorePrivate &p);
+
+	L_DECLARE_PRIVATE(Core);
+	L_DISABLE_COPY(Core);
+};
+
+LINPHONE_END_NAMESPACE
+
+#endif // ifndef _CORE_H_
