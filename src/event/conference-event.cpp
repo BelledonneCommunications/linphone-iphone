0e37546ef374ece527952b418591f9cefb0ccb21
Ronan Abhamon <ronan.abhamon@belledonne-communications.com>
2017-08-24 17:22:49 +0200

    feat(Event): add conference event

---
+++ src/event/conference-event.cpp
@@ -0,0 +1,54 @@
+/*
+ * conference-event.cpp
+ * Copyright (C) 2017  Belledonne Communications SARL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "event-p.h"
+
+#include "conference-event.h"
+
+// =============================================================================
+
+using namespace std;
+
+LINPHONE_BEGIN_NAMESPACE
+
+class ConferenceEventPrivate : public EventPrivate {
+public:
+	shared_ptr<Address> address;
+};
+
+// -----------------------------------------------------------------------------
+
+ConferenceEvent::ConferenceEvent (Type type, const shared_ptr<Address> &address) : Event(*new ConferenceEventPrivate, type) {
+	L_D(ConferenceEvent);
+	L_ASSERT(address);
+	d->address = address;
+}
+
+ConferenceEvent::ConferenceEvent (const ConferenceEvent &src) : ConferenceEvent(src.getType(), src.getAddress()) {}
+
+ConferenceEvent &ConferenceEvent::operator= (const ConferenceEvent &src) {
+	L_D(ConferenceEvent);
+	if (this != &src) {
+		Event::operator=(src);
+		d->address = src.getPrivate()->address;
+	}
+
+	return *this;
+}
+
+LINPHONE_END_NAMESPACE
---
+++ src/event/conference-event.h
@@ -0,0 +1,49 @@
+/*
+ * conference-event.h
+ * Copyright (C) 2017  Belledonne Communications SARL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _CONFERENCE_EVENT_H_
+#define _CONFERENCE_EVENT_H_
+
+#include <memory>
+
+#include "event.h"
+
+// =============================================================================
+
+LINPHONE_BEGIN_NAMESPACE
+
+class Address;
+class Conference;
+class ConferenceEventPrivate;
+
+class LINPHONE_PUBLIC ConferenceEvent : public Event {
+public:
+	ConferenceEvent (Type type, const std::shared_ptr<Address> &address);
+	ConferenceEvent (const ConferenceEvent &src);
+
+	ConferenceEvent &operator= (const ConferenceEvent &src);
+
+	std::shared_ptr<Address> getAddress () const;
+
+private:
+	L_DECLARE_PRIVATE(ConferenceEvent);
+};
+
+LINPHONE_END_NAMESPACE
+
+#endif // ifndef _CONFERENCE_EVENT_H_
