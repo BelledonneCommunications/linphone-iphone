d15826727711b7ec2a9f440e039735f0b8397576
Jehan Monnier <jehan.monnier@linphone.org>
2017-09-26 13:28:21 +0200

    fix xcode9 compilation issue

--- coreapi/call_log.c
+++ coreapi/call_log.c
@@ -16,8 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
-
+#ifndef __APPLE__
 #define _XOPEN_SOURCE 700 /*required for strptime of GNU libc*/
+#endif
 
 #include <time.h>
 #include "private.h"
