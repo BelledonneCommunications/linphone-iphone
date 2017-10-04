bb41aeb4deab9ceab72a5fbfe09d31e350ef2573
Benjamin Reis <benjamin.reis@belledonne-communications.com>
2017-10-04 11:48:17 +0200

    remove <<<<<HEAD

--- src/content/content.cpp
+++ src/content/content.cpp
@@ -71,7 +71,6 @@ Content &Content::operator= (Content &&src) {
 	return *this;
 }
 
-<<<<<<< HEAD
 bool Content::operator==(const Content& content) const {
 	L_D();
 	return d->contentType == content.getContentType() && d->body == content.getBody() && d->contentDisposition == content.getContentDisposition();
