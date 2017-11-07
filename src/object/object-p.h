e97502299a6ce62a90bccbe7f7d8172edd545e84
Ronan Abhamon <ronan.abhamon@belledonne-communications.com>
2017-11-07 15:51:46 +0100

    fix(object-p): add missing import

--- src/object/object-p.h
+++ src/object/object-p.h
@@ -24,6 +24,7 @@
 #include <unordered_map>
 
 #include "base-object-p.h"
+#include "object.h"
 #include "variant/variant.h"
 
 // =============================================================================
