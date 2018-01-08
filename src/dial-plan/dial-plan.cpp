588bea60b920634269cef3142d864e1deddca3bc
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2018-01-08 10:59:35 +0100

    Fixed Brazil dial plan (see issue #4440)

--- src/dial-plan/dial-plan.cpp
+++ src/dial-plan/dial-plan.cpp
@@ -64,7 +64,7 @@ const list<DialPlan> DialPlanPrivate::DialPlans = {
 	{ "Bolivia", "BO", "591", 8, "00" },
 	{ "Bosnia and Herzegovina", "BA", "387", 8, "00" },
 	{ "Botswana", "BW", "267", 8, "00" },
-	{ "Brazil", "BR", "55", 10, "00" },
+	{ "Brazil", "BR", "55", 11, "00" },
 	{ "Brunei Darussalam", "BN", "673", 7, "00" },
 	{ "Bulgaria", "BG", "359", 9, "00" },
 	{ "Burkina Faso", "BF", "226", 8, "00" },
