6c88e6be876f37eedf0138786d5cdf7a3bf2cd4b
Ronan Abhamon <ronan.abhamon@belledonne-communications.com>
2018-02-08 12:46:47 +0100

    feat(utils): add a BackgroundTask component

---
+++ src/utils/background-task.cpp
@@ -0,0 +1,48 @@
+/*
+ * background-task.cpp
+ * Copyright (C) 2010-2018 Belledonne Communications SARL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "background-task.h"
+#include "c-wrapper/internal/c-sal.h"
+#include "logger/logger.h"
+
+// =============================================================================
+
+LINPHONE_BEGIN_NAMESPACE
+
+static void notifier (void *context) {
+	static_cast<BackgroundTask *>(context)->handleTimeout();
+}
+
+void BackgroundTask::start () {
+	unsigned long newId = sal_begin_background_task(mName.c_str(), notifier, this);
+	lInfo() << "Starting background task [" << newId << "] with name : [" << mName << "].";
+	stop();
+	mId = newId;
+}
+
+void BackgroundTask::stop () {
+	if (mId == 0)
+		return;
+
+	lInfo() << "Ending background task [" << mId << "] with name : [" << mName << "].";
+	sal_end_background_task(mId);
+	mId = 0;
+}
+
+LINPHONE_END_NAMESPACE
