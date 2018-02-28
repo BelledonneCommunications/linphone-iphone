d41dba6357eb0f9617535409f59910176ddc157a
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2018-02-28 16:13:08 +0100

    Fixed message stored one time in clear and one time encrypted if using asynchronism while encrypting

--- src/chat/chat-message/chat-message.cpp
+++ src/chat/chat-message/chat-message.cpp
@@ -582,7 +582,7 @@ void ChatMessagePrivate::send () {
 
 	currentSendStep |= ChatMessagePrivate::Step::Started;
 
-	if (toBeStored)
+	if (toBeStored || currentSendStep == (ChatMessagePrivate::Step::Started | ChatMessagePrivate::Step::None))
 		storeInDb();
 
 	if ((currentSendStep & ChatMessagePrivate::Step::FileUpload) == ChatMessagePrivate::Step::FileUpload) {
