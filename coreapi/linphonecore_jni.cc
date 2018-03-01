5b13469eafbda3e5148b69e158aa58ce9d5dd66c
Erwan Croze <erwan.croze@belledonne-communications.com>
2018-03-01 11:53:39 +0100

    Fixing leak in JNI

--- coreapi/linphonecore_jni.cc
+++ coreapi/linphonecore_jni.cc
@@ -4772,6 +4772,7 @@ static void file_transfer_progress_indication(LinphoneChatMessage *msg, const Li
 	if (jmessage) {
 		env->DeleteLocalRef(jmessage);
 	}
+	env->DeleteLocalRef(listener);
 }
 
 static void file_transfer_recv(LinphoneChatMessage *msg, const LinphoneContent* content, const LinphoneBuffer *buffer) {
@@ -4801,6 +4802,7 @@ static void file_transfer_recv(LinphoneChatMessage *msg, const LinphoneContent* 
 	if (jmessage) {
 		env->DeleteLocalRef(jmessage);
 	}
+	env->DeleteLocalRef(listener);
 }
 
 static LinphoneBuffer* file_transfer_send(LinphoneChatMessage *msg,  const LinphoneContent* content, size_t offset, size_t size) {
@@ -4831,6 +4833,7 @@ static LinphoneBuffer* file_transfer_send(LinphoneChatMessage *msg,  const Linph
 
 	buffer = create_c_linphone_buffer_from_java_linphone_buffer(env, jbuffer);
 	env->DeleteLocalRef(jbuffer);
+	env->DeleteLocalRef(listener);
 	return buffer;
 }
 
