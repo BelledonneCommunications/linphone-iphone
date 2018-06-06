ce9cc56c6850f84fa744bf8295282c12a3b41921
David Idmansour <david.idmansour@belledonne-communications.com>
2018-06-06 15:52:38 +0200

    hello world widget

---
+++ latestCallsWidget/TodayViewController.h
@@ -0,0 +1,12 @@
+//
+//  TodayViewController.h
+//  latestCallsWidget
+//
+//  Created by David Idmansour on 06/06/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
