88a8820f958b3e33d62c27738f32ac859a7cd8ba
David Idmansour <david.idmansour@belledonne-communications.com>
2018-06-18 11:17:16 +0200

    created latest chatrooms widget target + interface

---
+++ latestChatroomsWidget/TodayViewController.h
@@ -0,0 +1,12 @@
+//
+//  TodayViewController.h
+//  latestChatroomsWidget
+//
+//  Created by David Idmansour on 18/06/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
