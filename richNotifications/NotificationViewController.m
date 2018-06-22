0eaccadef8ede65f26a50a61c92d86980399e5f9
David Idmansour <david.idmansour@belledonne-communications.com>
2018-06-22 10:22:12 +0200

    hello world rich notification

---
+++ richNotifications/NotificationViewController.m
@@ -0,0 +1,29 @@
+//
+//  NotificationViewController.m
+//  richNotifications
+//
+//  Created by David Idmansour on 22/06/2018.
+//
+
+#import "NotificationViewController.h"
+#import <UserNotifications/UserNotifications.h>
+#import <UserNotificationsUI/UserNotificationsUI.h>
+
+@interface NotificationViewController () <UNNotificationContentExtension>
+
+@property IBOutlet UILabel *label;
+
+@end
+
+@implementation NotificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any required interface initialization here.
+}
+
+- (void)didReceiveNotification:(UNNotification *)notification {
+    self.label.text = notification.request.content.body;
+}
+
+@end
