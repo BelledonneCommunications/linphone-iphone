9d822a95fc326e05a8e2b4e546bc4c4e7646258c
David Idmansour <david.idmansour@belledonne-communications.com>
2018-07-30 13:33:18 +0200

    video call from siri

--- siriExtension/IntentHandler.m
+++ siriExtension/IntentHandler.m
@@ -7,7 +7,7 @@
 
 #import "IntentHandler.h"
 
-@interface IntentHandler () <INSendMessageIntentHandling, INStartAudioCallIntentHandling>
+@interface IntentHandler () <INSendMessageIntentHandling, INStartAudioCallIntentHandling, INStartVideoCallIntentHandling>
 
 @end
 
@@ -157,6 +157,71 @@
 - (void)handleStartAudioCall:(nonnull INStartAudioCallIntent *)intent completion:(nonnull void (^)(INStartAudioCallIntentResponse * _Nonnull))completion {
     NSUserActivity *activity = [[NSUserActivity alloc] initWithActivityType:NSStringFromClass(INStartAudioCallIntent.class)];
     INStartAudioCallIntentResponse *response = [[INStartAudioCallIntentResponse alloc] initWithCode:INStartAudioCallIntentResponseCodeContinueInApp userActivity:activity];
+    completion(response);
+}
+
+#pragma mark - INStartVideoCallIntentHandling
+
+- (void)resolveContactsForStartVideoCall:(INStartVideoCallIntent *)intent withCompletion:(void (^)(NSArray<INPersonResolutionResult *> * _Nonnull))completion {
+    NSArray<INPerson *> *contacts = intent.contacts;
+    if (contacts.count == 0) {
+        completion(@[[INPersonResolutionResult needsValue]]);
+        return;
+    }
+    NSMutableArray<INPersonResolutionResult *> *responses = [NSMutableArray array];
+    NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:@"group.belledonne-communications.linphone.siri"];
+    NSDictionary<NSString *, NSArray *> *addresses = [defaults objectForKey:@"addresses"];
+    for (id iterator in contacts)
+        [responses addObject:[INPersonResolutionResult notRequired]];
+    NSString *contactName = [contacts[0].displayName lowercaseString];
+    NSMutableArray<INPerson *> *matchingContacts = [NSMutableArray array];
+    for (NSString *name in addresses.allKeys) {
+        if ([[name lowercaseString] containsString:[contactName lowercaseString]]) {
+            INPersonHandle *handle = [[INPersonHandle alloc] initWithValue:[addresses objectForKey:name].firstObject type:INPersonHandleTypeUnknown];
+            INPerson *contact = [[INPerson alloc] initWithPersonHandle:handle nameComponents:nil displayName:name image:nil contactIdentifier:nil customIdentifier:handle.value];
+            [matchingContacts addObject:contact];
+        }
+    }
+    INPersonResolutionResult *response;
+    switch(matchingContacts.count) {
+        case 0:
+            response = [INPersonResolutionResult unsupported];
+            break;
+        case 1:
+            response = [INPersonResolutionResult successWithResolvedPerson:matchingContacts.firstObject];
+            break;
+        default:{
+            BOOL match = NO;
+            for (INPerson *person in matchingContacts)
+                if ([person.displayName isEqualToString:contactName]) {
+                    response = [INPersonResolutionResult confirmationRequiredWithPersonToConfirm:person];
+                    match = YES;
+                }
+            if (!match)
+                response = [INPersonResolutionResult disambiguationWithPeopleToDisambiguate:matchingContacts];
+        }
+    }
+    [responses setObject:response atIndexedSubscript:0];
+    completion(responses);
+}
+
+- (void)confirmStartVideoCall:(INStartVideoCallIntent *)intent completion:(void (^)(INStartVideoCallIntentResponse * _Nonnull))completion {
+    NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:@"group.belledonne-communications.linphone.siri"];
+    NSNumber *loggedIn = [defaults objectForKey:@"loggedIn"];
+    if (!loggedIn || [loggedIn isEqualToNumber:@NO]) {
+        INStartVideoCallIntentResponse *response = [[INStartVideoCallIntentResponse alloc] initWithCode:INStartVideoCallIntentResponseCodeFailureAppConfigurationRequired userActivity:nil];
+        completion(response);
+        return;
+    }
+    
+    NSUserActivity *activity = [[NSUserActivity alloc] initWithActivityType:NSStringFromClass(INStartVideoCallIntent.class)];
+    INStartVideoCallIntentResponse *response = [[INStartVideoCallIntentResponse alloc] initWithCode:INStartVideoCallIntentResponseCodeReady userActivity:activity];
+    completion(response);
+}
+
+- (void)handleStartVideoCall:(nonnull INStartVideoCallIntent *)intent completion:(nonnull void (^)(INStartVideoCallIntentResponse * _Nonnull))completion {
+    NSUserActivity *activity = [[NSUserActivity alloc] initWithActivityType:NSStringFromClass(INStartVideoCallIntent.class)];
+    INStartVideoCallIntentResponse *response = [[INStartVideoCallIntentResponse alloc] initWithCode:INStartVideoCallIntentResponseCodeContinueInApp userActivity:activity];
     completion(response);
 }
 
