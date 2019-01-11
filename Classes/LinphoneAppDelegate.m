3bc738431f864f3564b383af58fba2f82979bbbf
Danmei Chen <danmei.chen@belledonne-communications.com>
2019-01-11 18:10:34 +0100

    add GoogleService-Info.plist into gitignore

--- Classes/LinphoneAppDelegate.m
+++ Classes/LinphoneAppDelegate.m
@@ -239,7 +239,12 @@
 #pragma deploymate pop
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [FIRApp configure];
+    NSString *pathForFile=[[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"GoogleService-Info.plist"];
+    if ([[NSFileManager defaultManager] fileExistsAtPath:pathForFile]){
+        // If GoogleService-Info.plist doesn't exist, not call this function avoiding a crash.
+        [FIRApp configure];
+    }
+    
     UIApplication *app = [UIApplication sharedApplication];
 	UIApplicationState state = app.applicationState;
 
