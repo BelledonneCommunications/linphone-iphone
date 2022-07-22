f8f9b997189d324cca0b952882df4b68ff5c448c
Quentin Monnier <quentin.monnier38@gmail.com>
2022-07-22 17:26:56 +0200

    bug fix: -resume the active call by clicking on the play button and not by clicking anywhere on the screen when it is paused

--- Classes/Swift/Voip/Views/CompositeViewControllers/ActiveCallOrConferenceView.swift
+++ Classes/Swift/Voip/Views/CompositeViewControllers/ActiveCallOrConferenceView.swift
@@ -121,15 +121,13 @@ import linphonesw
 		callPausedByRemoteView?.isHidden = true
 		
 		// Paused by local (Call)
-		callPausedByLocalView = PausedCallOrConferenceView(iconName: "voip_conference_play_big",titleText: VoipTexts.call_locally_paused_title,subTitleText: VoipTexts.call_locally_paused_subtitle)
+		callPausedByLocalView = PausedCallOrConferenceView(iconName: "voip_conference_play_big",titleText: VoipTexts.call_locally_paused_title,subTitleText: VoipTexts.call_locally_paused_subtitle, onClickAction: {
+            CallsViewModel.shared.currentCallData.value??.togglePause()
+        })
 		view.addSubview(callPausedByLocalView!)
 		callPausedByLocalView?.matchParentSideBorders().matchParentHeight().alignAbove(view:controlsView,withMargin:SharedLayoutConstants.buttons_bottom_margin).done()
 		callPausedByLocalView?.isHidden = true
-		callPausedByLocalView?.onClick {
-			CallsViewModel.shared.currentCallData.value??.togglePause()
-		}
-		
-		
+
 		
 		// Conference paused
 		conferencePausedView = PausedCallOrConferenceView(iconName: "voip_conference_play_big",titleText: VoipTexts.conference_paused_title,subTitleText: VoipTexts.conference_paused_subtitle)
