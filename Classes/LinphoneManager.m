a4bfa37454fa014b6ad32a9f47d9185495107d92
QuentinArguillere <quentin.arguillere@belledone-communications.com>
2022-08-29 17:11:29 +0200

    Remove line committed by mistake

--- Classes/LinphoneManager.m
+++ Classes/LinphoneManager.m
@@ -1423,7 +1423,6 @@ void popup_link_account_cb(LinphoneAccountCreator *creator, LinphoneAccountCreat
 
 	[ConfigManager.instance setDbWithDb:_configDb];
 	[CallManager.instance setCoreWithCore:theLinphoneCore];
-	[MagicSearchSingleton.instance setupCernLdapWithLc:[LinphoneManager getLc]]; // CERN
 	
 	[LinphoneManager.instance startLinphoneCore];
 
