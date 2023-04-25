88e497a783aa87c09317fe61a6743edd2f6e5420
Benoit Martins <benoit.martins@belledonne-communications.com>
2023-04-25 13:06:29 +0200

    Remove print in Avatar Class

--- Classes/Swift/Voip/Widgets/Avatar.swift
+++ Classes/Swift/Voip/Widgets/Avatar.swift
@@ -150,7 +150,6 @@ class Avatar : UIView {
 			friend.append(Friend.getSwiftObject(cObject: (contactAddress.friend)!))
 			let newFriendDelegate = FriendDelegateStub(
 				onPresenceReceived: { (linphoneFriend: Friend) -> Void in
-					print("onPresenceReceivedonPresenceReceived \(linphoneFriend.address?.displayName)")
                     if (linphoneFriend.address?.asStringUriOnly()) != nil {
                         let presenceModel = linphoneFriend.getPresenceModelForUriOrTel(uriOrTel: (linphoneFriend.address?.asStringUriOnly())!)
                         NotificationCenter.default.post(name: Notification.Name("LinphoneFriendPresenceUpdate"), object: nil, userInfo: ["friend": linphoneFriend.address?.asStringUriOnly() ?? "", "isOnline": presenceModel!.consolidatedPresence.rawValue == LinphoneConsolidatedPresenceOnline.rawValue])
