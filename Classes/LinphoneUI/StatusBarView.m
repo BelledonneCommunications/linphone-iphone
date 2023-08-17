c58cbad0026b98119696864b432b2a43b4efaf5f
QuentinArguillere <quentin.arguillere@belledone-communications.com>
2023-08-17 16:12:01 +0200

    Temporarely revert "Add LinphoneRegistrationRefreshing status to the registration state switches to avoid crash" because the 5.3.0 SDK does not have this enum yet.
    This reverts commit 1192b8090628a385de70856b3d1250b0f7eae479.

--- Classes/LinphoneUI/StatusBarView.m
+++ Classes/LinphoneUI/StatusBarView.m
@@ -176,7 +176,6 @@
 		case LinphoneRegistrationNone:
 			return [UIImage imageNamed:@"led_disconnected.png"];
 		case LinphoneRegistrationProgress:
-		case LinphoneRegistrationRefreshing:
 			return [UIImage imageNamed:@"led_inprogress.png"];
 		case LinphoneRegistrationOk:
 			return [UIImage imageNamed:@"led_connected.png"];
@@ -208,7 +207,6 @@
 
 		switch (state) {
 			case LinphoneRegistrationOk:
-			case LinphoneRegistrationRefreshing:
 				message = NSLocalizedString(@"Connected", nil);
 				break;
 			case LinphoneRegistrationNone:
