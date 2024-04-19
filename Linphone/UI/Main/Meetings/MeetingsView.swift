20f7d54c01faa53a71347af05fbaa312d4ab1a90
QuentinArguillere <quentin.arguillere@belledone-communications.com>
2024-04-19 15:34:05 +0200

    Add skeleton for MeetingsView MeetingsFragment

---
+++ Linphone/UI/Main/Meetings/MeetingsView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingsView.swift
+//  Linphone
+//
+//  Created by QuentinArguillere on 18/04/2024.
+//
+
+import SwiftUI
+
+struct MeetingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingsView()
+}
