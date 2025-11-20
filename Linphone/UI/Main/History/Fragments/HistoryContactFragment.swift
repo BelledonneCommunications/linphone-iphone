fa1f8386b4b541fd1ef903661d0796961649a510
Benoit Martins <benoit.martins@belledonne-communications.com>
2025-11-20 17:49:14 +0100

    Refresh presence info in history detail

--- Linphone/UI/Main/History/Fragments/HistoryContactFragment.swift
+++ Linphone/UI/Main/History/Fragments/HistoryContactFragment.swift
@@ -196,14 +196,8 @@ struct HistoryContactFragment: View {
 											.padding(.top, 5)
 									}
 									
-									if let avatarModel = historyModel.avatarModel {
-										Text(avatarModel.lastPresenceInfo)
-											.foregroundStyle(avatarModel.lastPresenceInfo == "Online" ? Color.greenSuccess500 : Color.orangeWarning600)
-											.multilineTextAlignment(.center)
-											.default_text_style_300(styleSize: 12)
-											.frame(maxWidth: .infinity)
-											.frame(height: 20)
-											.padding(.top, 5)
+									if let avatar = historyModel.avatarModel {
+										AvatarPresenceView(avatarModel: avatar)
 									} else {
 										Text("")
 											.multilineTextAlignment(.center)
@@ -422,6 +416,21 @@ struct HistoryContactFragment: View {
 		.navigationViewStyle(.stack)
 	}
 }
+
+struct AvatarPresenceView: View {
+	@ObservedObject var avatarModel: ContactAvatarModel
+
+	var body: some View {
+		Text(avatarModel.lastPresenceInfo)
+			.foregroundStyle(avatarModel.lastPresenceInfo == "Online" ? Color.greenSuccess500 : Color.orangeWarning600)
+			.multilineTextAlignment(.center)
+			.default_text_style_300(styleSize: 12)
+			.frame(maxWidth: .infinity)
+			.frame(height: 20)
+			.padding(.top, 5)
+	}
+}
+
 
 #Preview {
 	HistoryContactFragment(
