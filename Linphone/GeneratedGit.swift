4feae7fccd952ebc8fd063e5f1f3e4de477a5eea
Benoit Martins <benoit.martins@belledonne-communications.com>
2025-12-18 09:54:30 +0100

    Temporarily disable queued and pendingDelivery message handling awaiting SDK fix

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "master"
-    public static let commit = "84ec09173"
+    public static let commit = "1501dae01"
     public static let tag = "6.1.0-alpha"
 }
