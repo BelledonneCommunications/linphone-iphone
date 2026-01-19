b348307b6c17db2c3a21cb6b42f2eaea4d09ec52
Benoit Martins <benoit.martins@belledonne-communications.com>
2026-01-19 11:07:34 +0100

    Update SPM dependencies

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "master"
-    public static let commit = "986276c04"
+    public static let commit = "c5c970d17"
     public static let tag = "6.1.0-alpha"
 }
