9d78fc2e29b01a9a7fe2d0a604910bfbc78a08e0
Benoit Martins <benoit.martins@belledonne-communications.com>
2026-03-06 16:07:27 +0100

    Add AppIntentVocabulary.plist for INStartCallIntent example phrases

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "master"
-    public static let commit = "d00257d93"
+    public static let commit = "d0418ae07"
     public static let tag = "6.1.0-alpha"
 }
