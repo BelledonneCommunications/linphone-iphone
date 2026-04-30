11fb53da31875420051f252d686b3040adb9244b
Benoit Martins <benoit.martins@belledonne-communications.com>
2026-04-30 15:31:50 +0200

    Fix LDAP password editing in settings

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "release/6.2"
-    public static let commit = "924dc8b03"
+    public static let commit = "c180fb120"
     public static let tag = "6.2.0-beta"
 }
