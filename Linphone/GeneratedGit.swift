5fc817849f1a1123575f22d26c6c9befe02a7eb8
Benoit Martins <benoit.martins@belledonne-communications.com>
2026-05-21 11:53:11 +0200

    Delete delegate removal when LinkPhoneNumberUsingCode is triggered

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "release/6.2"
-    public static let commit = "8e59a33ce"
+    public static let commit = "e3393e1d8"
     public static let tag = "6.2.0-beta"
 }
