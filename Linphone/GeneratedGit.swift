2af9951cd1599fcc1ccdaf6eeaa9599c6c4a9db7
Benoit Martins <benoit.martins@belledonne-communications.com>
2026-05-28 15:09:37 +0200

    Disable validate button when creating a contact if the first name is empty

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "release/6.2"
-    public static let commit = "8417abe59"
+    public static let commit = "72f79c20b"
     public static let tag = "6.2.0-beta"
 }
