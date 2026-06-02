e4728149d53c0b9a178e86c2682637842fb0a33f
Benoit Martins <benoit.martins@belledonne-communications.com>
2026-06-02 11:58:24 +0200

    Change branch and commit values in GeneratedGit

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum AppGitInfo {
-    public static let branch = "release/6.2"
-    public static let commit = "7d4845587"
-    public static let tag = "6.2.0"
+    public static let branch = "master"
+    public static let commit = "7011597b3"
+    public static let tag = "6.3.0-alpha"
 }
