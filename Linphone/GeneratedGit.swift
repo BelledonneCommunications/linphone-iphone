26c0eb89a1144410b577eab21cdfd1a67e852f9e
Benoit Martins <benoit.martins@belledonne-communications.com>
2026-07-07 12:43:11 +0200

    Updated CHANGELOG & bumped version code to 6.2.1 (2)

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "release/6.2"
-    public static let commit = "ce248310b"
+    public static let commit = "be7e8560c"
     public static let tag = "6.2.0"
 }
