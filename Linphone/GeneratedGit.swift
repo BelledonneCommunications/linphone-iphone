da4ee24d7b4726e2ba79c85773e74b9df2cf60f5
Benoit Martins <benoit.martins@belledonne-communications.com>
2026-09-14 17:21:00 +0200

    Run forwarded message send on core queue instead of main thread

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "fix/misc-fixes"
-    public static let commit = "cb243942c"
+    public static let commit = "c0d471f31"
     public static let tag = "6.2.3"
 }
